0691a539bec1317de71facf537481eea84be1e6c
marwan-b <bouchrikamarvin@gmail.com>
2023-12-20 16:35:02 +0100

    Cors allowedOrigins Api

--- back/index.js
+++ back/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const port = 5000;
-
+const cors = require('cors');
 const app = express();
+
+const allowedOrigins = ['http://localhost:3000'];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+};
 
 app.use('/api/users', require('./routes/users.routes'));
 
