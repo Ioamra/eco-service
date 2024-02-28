61365b3a717eb171c9295aaf0b136892a04ac725
Jadorelethe <vlad.hayevskyy@gmail.com>
2024-02-28 18:05:30 +0100

    navbar a finir

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
 import LoginScreen from './pages/sing-in-up';
 import './index.css';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from './components/footer/footer'; 
+import Navbar from './components/navbar/navbar';
 function App() {
   return (
+    <Router>
       <div>
+        <Navbar>
+          <Routes>
+          <Route path="/"/>
+          <Route path="/"/>
+          <Route path="/"/>
+          <Route path="/"/>
+        </Routes>
+        </Navbar>
         <main>
           <LoginScreen/>
         </main>
         <Footer />
       </div>
+    </Router>
   );
 }
 
