4267ccdd3feb43af9d98b6d6a8cac5b9105a1327
ioamra <iom.monchau@gmail.com>
2024-02-29 14:19:02 +0100

    mini maj navbar

--- front/src/components/navbar/navbar.jsx
+++ front/src/components/navbar/navbar.jsx
@@ -5,10 +5,13 @@ import EcoServiceLogo from './logoEcoService.png';
 import LogoPanier from './logopanier.png';
 import LogoLogin from './logologin.png';
 import Modal from './modal.jsx';
+import { getPanier } from '../../services/commande.js';
 
 const Navbar = () => {
   const [showModal, setShowModal] = useState(false);
   const [showMenu, setShowMenu] = useState(false); 
+
+  // const panier = getPanier();
 
   const handleOpenModal = () => {
     setShowModal(true);
@@ -35,9 +38,9 @@ const Navbar = () => {
           <NavLink className="nav-item" to="/notre-demarche" activeClassName="active">NOTRE DEMARCHE</NavLink>
           <NavLink className="nav-item" to="/nos-tutoriels" activeClassName="active">NOS TUTORIELS</NavLink>
         </div>
-      </div>
-      <div className="search-container mobile-search">
-        <input className="search-bar" type="text" placeholder="Search..."/>
+        <div className="search-container mobile-search">
+          <input className="search-bar" type="text" placeholder="Search..."/>
+        </div>
       </div>
       <div className="icon-container">
         <button onClick={handleOpenModal} className="icon-button panier">
